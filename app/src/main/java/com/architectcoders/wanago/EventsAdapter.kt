834b8eb30508e55caad696d2a767755773b89b36
DevDCruz <juandecruzsan@gmail.com>
2022-10-28 11:39:52 +0200

    Creación de recyclerview
    He creado un recyclerview en el activitymain que recibe la información manualmente.
    He implementado el viewbinding y la librería glade.

---
+++ app/src/main/java/com/architectcoders/wanago/EventsAdapter.kt
@@ -0,0 +1,38 @@
+package com.architectcoders.wanago
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.architectcoders.wanago.databinding.ItemEventBinding
+import com.bumptech.glide.Glide
+
+class EventsAdapter(private val events: List<Event>): RecyclerView.Adapter<EventsAdapter.ViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemEventBinding.inflate(
+            LayoutInflater
+            .from(parent.context),
+            parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(events[position])
+    }
+
+    override fun getItemCount() = events.size
+
+    class ViewHolder(private val binding: ItemEventBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(event: Event){
+            binding.tVArtistName.text = event.artistName
+            binding.tVPlace.text = event.place
+            Glide
+                .with(binding.root.context)
+                .load(event.poster)
+                .into(binding.iVPoster)
+        }
+    }
+}
--- app/src/main/java/com/architectcoders/wanago/MainActivity.kt
+++ app/src/main/java/com/architectcoders/wanago/MainActivity.kt
@@ -2,10 +2,22 @@ package com.architectcoders.wanago
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.architectcoders.wanago.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerEventsList.adapter = EventsAdapter(listOf(
+            Event("https://loremflickr.com/320/240?lock=1", "Drake","Sevilla"),
+            Event("https://loremflickr.com/320/240?lock=", "Drake","Madrid"),
+            Event("https://loremflickr.com/320/240?lock=3", "Drake","Barcelona"),
+            Event("https://loremflickr.com/320/240?lock=4", "Drake","Oviedo"),
+            Event("https://loremflickr.com/320/240?lock=5", "Drake","Valencia")
+        )
+        )
     }
 }
