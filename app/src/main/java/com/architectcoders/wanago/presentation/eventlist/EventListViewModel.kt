976529b23efce51d9c9448e5bb17582e75503dff
Darío Martín <dariomartinlara@gmail.com>
2023-02-15 23:23:45 +0100

    Rename EventsRepository.kt

--- app/src/main/java/com/architectcoders/wanago/presentation/eventlist/EventListViewModel.kt
+++ app/src/main/java/com/architectcoders/wanago/presentation/eventlist/EventListViewModel.kt
@@ -4,11 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.architectcoders.wanago.data.repository.EventRepository
+import com.architectcoders.wanago.data.repository.EventsRepository
 import com.architectcoders.wanago.domain.model.Event
 import kotlinx.coroutines.launch
 
-class EventListViewModel(private val eventRepository: EventRepository) : ViewModel() {
+class EventListViewModel(private val eventRepository: EventsRepository) : ViewModel() {
     private val _events = MutableLiveData<List<Event>>()
     val events: LiveData<List<Event>> get() = _events
 
--- app/src/main/java/com/architectcoders/wanago/presentation/eventlist/EventListViewModelFactory.kt
+++ app/src/main/java/com/architectcoders/wanago/presentation/eventlist/EventListViewModelFactory.kt
@@ -2,9 +2,10 @@ package com.architectcoders.wanago.presentation.eventlist
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.architectcoders.wanago.data.repository.EventRepository
+import com.architectcoders.wanago.data.repository.EventsRepository
 
-class EventListViewModelFactory(private val eventRepository: EventRepository) :
+@Suppress("UNCHECKED_CAST")
+class EventListViewModelFactory(private val eventRepository: EventsRepository) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return EventListViewModel(eventRepository) as T
