69ea631603611b13c56c190f3e26f40e601f98e3
NachoGoro <nachogoro.dev@gmail.com>
2023-02-17 19:43:31 +0100

    Build after rebase on top of develop

--- data/src/main/java/com/architectcoders/wanago/data/datasource/EventsRemoteDataSource.kt
+++ data/src/main/java/com/architectcoders/wanago/data/datasource/EventsRemoteDataSource.kt
@@ -1,6 +1,6 @@
 package com.architectcoders.wanago.data.datasource
 
-import com.architectcoders.wanago.domain.model.Event
+import com.architectcoders.wanago.domain.Event
 
 interface EventsRemoteDataSource {
     suspend fun findNearbyEvents(region: String): List<Event>
