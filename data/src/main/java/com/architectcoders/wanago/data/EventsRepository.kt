4733de6bea77ddd11267a9b2ec2272c6af9c437f
Darío Martín <dariomartinlara@gmail.com>
2023-02-20 09:38:30 +0100

    Save favorites

--- data/src/main/java/com/architectcoders/wanago/data/EventsRepository.kt
+++ data/src/main/java/com/architectcoders/wanago/data/EventsRepository.kt
@@ -1,11 +1,15 @@
 package com.architectcoders.wanago.data
 
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
 import com.architectcoders.wanago.data.datasource.EventsLocalDataSource
 import com.architectcoders.wanago.data.datasource.EventsRemoteDataSource
 import com.architectcoders.wanago.domain.WanagoError
 import com.architectcoders.wanago.domain.WanagoEvent
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
@@ -19,8 +23,22 @@ class EventsRepository @Inject constructor(
         it.filter { event -> event.isFavorite }
     }
 
-    suspend fun requestNearbyEvents(): Flow<PagingData<WanagoEvent>> {
-        return remoteDataSource.findNearbyEvents(regionRepository.findLastRegion())
+    suspend fun requestNearbyEvents(coroutineScope: CoroutineScope): Flow<PagingData<WanagoEvent>> {
+        val favoriteEventsFlow = getFavoriteEvents()
+
+        val nearbyEventsFlow = remoteDataSource.findNearbyEvents(regionRepository.findLastRegion())
+            .cachedIn(coroutineScope)
+
+        val combinedFlow =
+            nearbyEventsFlow.combine(favoriteEventsFlow) { remoteEvents, favoriteEvents ->
+                val combinedEvents = remoteEvents.map { remoteEvent ->
+                    val isFavorite = favoriteEvents.any { it.id == remoteEvent.id }
+                    remoteEvent.copy(isFavorite = isFavorite)
+                }
+                combinedEvents
+            }
+
+        return combinedFlow
     }
 
     fun getEventById(id: String): Flow<WanagoEvent> = localDataSource.getById(id)
