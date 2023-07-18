38f99815fbad8ce5dc37a363fdd102f24f45bba9
NachoGoro <nachogoro.dev@gmail.com>
2023-07-18 15:49:01 +0200

    Add testShared module for common test data

---
+++ testShared/src/main/java/com/architectcoders/wanago/testshared/samples.kt
@@ -0,0 +1,5 @@
+package com.architectcoders.wanago.testshared
+
+import com.architectcoders.wanago.domain.WanagoEvent
+
+val sampleEvent = WanagoEvent("0", "Lichess Title Arena", "url", "0", "24/10/1992", "", true)
