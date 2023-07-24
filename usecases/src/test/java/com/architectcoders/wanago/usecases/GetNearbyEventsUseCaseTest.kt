2c827c8c7cd472646e999fd0606ef13296b19935
Darío Martín <dariomartinlara@gmail.com>
2023-07-25 00:37:37 +0200

    Add usecases tests

--- usecases/src/test/java/com/architectcoders/wanago/usecases/GetNearbyEventsUseCaseTest.kt
+++ usecases/src/test/java/com/architectcoders/wanago/usecases/GetNearbyEventsUseCaseTest.kt
@@ -5,21 +5,24 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestScope
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.verify
 
 @ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
 internal class GetNearbyEventsUseCaseTest {
 
-  @Test
-  fun `when use case is invoked then events repository is called`(): Unit = runBlocking {
-    val repository: EventsRepository = mock()
-    val getNearbyEventsUseCase = GetNearbyEventsUseCase(repository)
-    val testScope = TestScope()
+    @Test
+    fun `when use case is invoked then events repository is called`(): Unit = runBlocking {
+        val repository: EventsRepository = mock()
+        val getNearbyEventsUseCase = GetNearbyEventsUseCase(repository)
+        val testScope = TestScope()
 
-    getNearbyEventsUseCase(testScope)
+        getNearbyEventsUseCase(testScope)
 
-    verify(repository).requestNearbyEvents(testScope)
-  }
+        verify(repository).requestNearbyEvents(testScope)
+    }
 
 }
--- usecases/src/test/java/com/architectcoders/wanago/usecases/SwitchEventFavoriteUseCaseTest.kt
+++ usecases/src/test/java/com/architectcoders/wanago/usecases/SwitchEventFavoriteUseCaseTest.kt
@@ -1,12 +1,19 @@
 package com.architectcoders.wanago.usecases
 
 import com.architectcoders.wanago.data.EventsRepository
+import com.architectcoders.wanago.domain.WanagoError
 import com.architectcoders.wanago.testshared.sampleEvent
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 
+@RunWith(MockitoJUnitRunner::class)
 internal class SwitchEventFavoriteUseCaseTest {
 
     @Test
@@ -18,5 +25,21 @@ internal class SwitchEventFavoriteUseCaseTest {
         switchEventFavoriteUseCase(event)
 
         verify(repository).switchFavorite(event)
+    }
+
+    @Test
+    fun `when repository returns error then use case returns error`(): Unit = runBlocking {
+        val eventId = "1"
+        val errorMessage = "Error message"
+        val event = sampleEvent.copy(id = eventId)
+        val repository: EventsRepository = mock {
+            onBlocking { switchFavorite(event) } doReturn WanagoError.Unknown(errorMessage)
+        }
+        val switchEventFavoriteUseCase = SwitchEventFavoriteUseCase(repository)
+
+        val result: WanagoError? = switchEventFavoriteUseCase(event)
+
+        assertTrue(result is WanagoError.Unknown)
+        assertEquals(errorMessage, (result as WanagoError.Unknown).message)
     }
 }
