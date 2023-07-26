7d22f8cec81160c73a1b542feba8edd0cee6e88f
Darío Martín <dariomartinlara@gmail.com>
2023-07-27 00:30:25 +0200

    Add repository tests

--- usecases/src/test/java/com/architectcoders/wanago/usecases/GetEventByIdUseCaseTest.kt
+++ usecases/src/test/java/com/architectcoders/wanago/usecases/GetEventByIdUseCaseTest.kt
@@ -1,12 +1,17 @@
 package com.architectcoders.wanago.usecases
 
 import com.architectcoders.wanago.data.EventsRepository
+import com.architectcoders.wanago.testshared.sampleEvent
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 
 @ExperimentalCoroutinesApi
@@ -22,4 +27,19 @@ internal class GetEventByIdUseCaseTest {
 
         verify(repository).getEventById(eventId)
     }
+
+    @Test
+    fun `when use case is invoked with an eventId then repository returns the event`(): Unit =
+        runTest {
+            val eventId = "1"
+            val event = sampleEvent.copy(id = eventId)
+            val repository: EventsRepository = mock {
+                onBlocking { getEventById(eventId) } doReturn flowOf(event)
+            }
+            val getEventByIdUseCase = GetEventByIdUseCase(repository)
+
+            val result = getEventByIdUseCase(eventId)
+
+            assertEquals(result.first(), event)
+        }
 }
